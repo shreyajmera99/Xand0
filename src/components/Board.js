f133a2c26a759fc4e92fc41f0a98092a4cb59884
shreyajmera99 <ajmerashrey83@gmail.com>
2023-07-22 18:23:54 +0530

    Changes debugged

--- src/components/Board.js
+++ src/components/Board.js
@@ -11,14 +11,15 @@ export default function Board() {
   let status;
 
   if (winner) {
-    status = "Hurray Winner is : " + winner;
+    status = "Hurray Winner is: " + winner;
   } else if (isDraw) {
     status = "Game Draw";
   } else {
     status = "Next Player: " + (xNext ? "X" : "O");
   }
+
   const handleClick = (i) => {
-    if (squares[i]) {
+    if (squares[i] || winner) {
       return;
     }
     const nextsquare = squares.slice();
@@ -26,44 +27,92 @@ export default function Board() {
     if (xNext) {
       nextsquare[i] = "X";
     } else {
-      nextsquare[i] = "0";
+      nextsquare[i] = "O";
     }
     setSquares(nextsquare);
     setxIsNext(!xNext);
-    console.log(nextsquare);
   };
 
-  const resetgame = () => {
+  const resetGame = () => {
     setSquares(Array(9).fill(null));
+    setxIsNext(true);
   };
+
+  const isSquareDisabled = (i) => {
+    return winner || squares[i] !== null
+    
+   
+  };
+
   return (
     <>
       <nav className="navbarr">| X and O |</nav>
       <div className="maindiv">
         <div className="boardrow">
-          <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-          <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-          <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+          <Square
+            value={squares[0]}
+            onSquareClick={() => handleClick(0)}
+            disabled={isSquareDisabled(0)}
+          />
+          <Square
+            value={squares[1]}
+            onSquareClick={() => handleClick(1)}
+            disabled={isSquareDisabled(1)}
+          />
+          <Square
+            value={squares[2]}
+            onSquareClick={() => handleClick(2)}
+            disabled={isSquareDisabled(2)}
+          />
         </div>
         <div className="boardrow">
-          <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-          <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-          <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+          <Square
+            value={squares[3]}
+            onSquareClick={() => handleClick(3)}
+            disabled={isSquareDisabled(3)}
+          />
+          <Square
+            value={squares[4]}
+            onSquareClick={() => handleClick(4)}
+            disabled={isSquareDisabled(4)}
+          />
+          <Square
+            value={squares[5]}
+            onSquareClick={() => handleClick(5)}
+            disabled={isSquareDisabled(5)}
+          />
         </div>
         <div className="boardrow">
-          <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-          <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-          <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+          <Square
+            value={squares[6]}
+            onSquareClick={() => handleClick(6)}
+            disabled={isSquareDisabled(6)}
+          />
+          <Square
+            value={squares[7]}
+            onSquareClick={() => handleClick(7)}
+            disabled={isSquareDisabled(7)}
+          />
+          <Square
+            value={squares[8]}
+            onSquareClick={() => handleClick(8)}
+            disabled={isSquareDisabled(8)}
+          />
         </div>
-        <div className="status">{status} </div>
+        <div className="status">{status}</div>
 
         <div className="btnclass">
-        <button className="resetbtn" onClick={()=>resetgame()}>Reset Name </button>
+          <button className="resetbtn" onClick={resetGame}>
+            Reset Name
+          </button>
         </div>
       </div>
     </>
   );
 }
+
+// ... (calculatewinner function remains unchanged)
+
 
 const calculatewinner = (squares) => {
   const lines = [
